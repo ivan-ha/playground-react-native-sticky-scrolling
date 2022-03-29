721755ad1785c3d8991e475ab58498163927ab01
Ivan Ha <hangolam@gmail.com>
2022-03-30 00:17:53 +0800

    chore: init App with a simple FlatList

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, FlatList } from 'react-native';
 
-export default function App() {
+const data = [...Array(20).keys()].map(i => ({
+  id: i,
+  text: `Item ${i}`
+}))
+
+const Item = ({ text }) => (
+  <View style={styles.item}>
+    <Text style={styles.text}>{text}</Text>
+  </View>
+);
+
+const App = () => {
+  const renderItem = ({ item }) => (
+    <Item text={item.text} />
+  );
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +34,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 10,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+  },
+  text: {
+    fontSize: 20,
   },
 });
+
+export default App
